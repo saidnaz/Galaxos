b99bca40a346f003ab8358591e8b22b9147fcd9f
mohamed-bh <m.benhamadi@gmail.com>
2021-12-22 11:43:18 +0100

    First commit : structure projet à vide

---
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Application.java
@@ -0,0 +1,26 @@
+package fr.isika.cda.galaxos.model;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Entité de test qui peut être supprimée.
+ */
+@Entity
+public class Application {
+
+	@Id
+	private UUID id = UUID.randomUUID();
+	private String appName = "Galaxos App";
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+}
