b7fdf3670b41fcde9914b5e7d40f16246e4d1307
Valentin Zawartoski <val.zawar@gmail.com>
2021-12-23 17:12:25 +0100

    ClassComptUser
    Ajout de commentaire et modification

--- galaxos-app/src/main/java/fr/isika/cda/galaxos/model/ComptUser.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/ComptUser.java
@@ -4,8 +4,6 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -13,76 +11,69 @@ import javax.persistence.OneToOne;
 @Entity
 public class ComptUser {
 
-	
 	@Id
 	private String email;
-	
-	@Column(length = 20,name="motdepasse", nullable=false)
+
+	@Column(length = 20, name = "motdepasse", nullable = false)
 	private String mdp;
 
-	@Column(name="CompteValide",nullable= false)
-	private boolean isAccountValide=true;
+	@Column(name = "CompteValide", nullable = false)
+	private boolean isAccountValide = true;
+
+	@Column(name = "CompteClose", nullable = false)
+	private boolean isAccountNotValide = true;
+
 	
-	@Column(name="CompteClose", nullable=false)
-	private boolean isAccountNotValide=true;
-	
+	//Vas rejoindre la column fkAdherant 
 	@OneToOne
-	@JoinColumn(name="fkAdherent")
+	@JoinColumn(name = "fkAdherent")
 	private Adherent adherent;
-	
-	
 
-
+	
+	//GET & SET
 	public String getMdp() {
 		return mdp;
 	}
-
 
 	public void setMdp(String mdp) {
 		this.mdp = mdp;
 	}
 
-
 	public boolean isAccountValide() {
 		return isAccountValide;
 	}
-
 
 	public void setAccountValide(boolean isAccountValide) {
 		this.isAccountValide = isAccountValide;
 	}
 
-
 	public boolean isAccountNotValide() {
 		return isAccountNotValide;
 	}
-
 
 	public void setAccountNotValide(boolean isAccountNotValide) {
 		this.isAccountNotValide = isAccountNotValide;
 	}
 
-
 	public Adherent getAdherent() {
 		return adherent;
 	}
-
 
 	public void setAdherent(Adherent adherent) {
 		this.adherent = adherent;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
 
-
+	
+	//hashcode et equald pour le mail et mdp
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, mdp);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -95,11 +86,21 @@ public class ComptUser {
 		ComptUser other = (ComptUser) obj;
 		return Objects.equals(email, other.email) && Objects.equals(mdp, other.mdp);
 	}
-	
 
+	
+	//Constructeur à vide
+	
 	public ComptUser() {
 		super();
 	}
+
 	
+	//Methode to string pour afficher
 	
+	@Override
+	public String toString() {
+		return "ComptUser [email=" + email + ", mdp=" + mdp + ", isAccountValide=" + isAccountValide
+				+ ", isAccountNotValide=" + isAccountNotValide + ", adherent=" + adherent + "]";
+	}
+
 }
