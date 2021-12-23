69463732259c3cdb62e0a3037ed18fc4eb97d8e3
Philippe <p.agaciak@hotmail.fr>
2021-12-23 12:34:35 +0100

    Classe Profil et update Classe Adresse

--- galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Adresse.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Adresse.java
@@ -1,26 +1,122 @@
 package fr.isika.cda.galaxos.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Adresse {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.AUTO)
-	private Long idAdresse;
+	private Long id;
 	
-	@Column(name="Adresse", nullable=false)
-	private String adresse;
+	@Column(name="Numéro", nullable=false)
+	private String numero;
 	
-	@Column(name="Ville", nullable=false)
+	@Column(name="Libellé", nullable=false)
+	private String libelle;
+	
+	@Column(name="Ville", length=60, nullable=false)
 	private String ville;
 	
 	@Column(name="Code_Postal", nullable=false)
-	private Long codePostal;
+	private Integer codePostal;
+	
+//	@OneToOne(mappedBy = "adresse")
+//	 private Profil profil;
+	
+	public Adresse() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public Integer getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(Integer codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("Adresse [idAresse=");
+		builder.append(id);
+		
+		builder.append(", numéro=");
+		builder.append(numero);
+		
+		builder.append(", libellé=");
+		builder.append(libelle);
+		
+		builder.append(", ville=");
+		builder.append(ville);
+		
+		builder.append(", code postal=");
+		builder.append(codePostal);
+
+		builder.append("]");
+		
+		return builder.toString();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Adresse other = (Adresse) obj;
+		return Objects.equals(id, other.id) && Objects.equals(numero, other.numero) && 
+				Objects.equals(libelle, other.libelle) && Objects.equals(ville, other.ville)
+				&& Objects.equals(codePostal, other.codePostal);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, numero, libelle, ville, codePostal);
+	}
 	
 	
 }
---
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Profil.java
@@ -0,0 +1,193 @@
+package fr.isika.cda.galaxos.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Profil {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Basic(optional = false)
+	private String pseudo;
+	
+	@Column(length = 100, nullable=false)
+	private String nom;
+
+	@Column(length = 100, nullable=false)
+	private String prenom;
+
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	
+	@Size(min = 10, max = 12)
+    @Digits(fraction = 0, integer = 12)
+    private String telephone;
+	
+	@Lob
+	private String description;
+
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
+	private Byte[] photo;
+	
+	@OneToOne
+	@JoinColumn(name = "fkAdresse")
+	private Adresse adresse;
+	
+//	@OneToOne
+//	@JoinColumn(name = fk_adherent)
+//	private Adherent adherent;
+	
+	public Profil() {	
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Byte[] photo) {
+		this.photo = photo;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+//	public Adherent getDestinataire() {
+//		return destinataire;
+//	}
+//
+//	public Adherent getExpediteur() {
+//		return expediteur;
+//	}
+
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("Profil [id=");
+		builder.append(id);
+		
+		builder.append(", pseudo=");
+		builder.append(pseudo);
+		
+		builder.append(", nom=");
+		builder.append(nom);
+		
+		builder.append(", prenom=");
+		builder.append(prenom);
+		
+		builder.append(", date de naissance=");
+		builder.append(dateNaissance);
+		
+		builder.append(", téléphone=");
+		builder.append(telephone);
+		
+		builder.append(", descriptionn=");
+		builder.append(description);
+		
+		builder.append(", photo=");
+		builder.append(photo);
+		
+		builder.append("]");
+		
+		return builder.toString();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Profil other = (Profil) obj;
+		return Objects.equals(id, other.id) && Objects.equals(pseudo, other.pseudo)
+				&& Objects.equals(nom, other.nom) && Objects.equals(prenom, other.prenom);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, pseudo, nom, prenom);
+	}
+
+}
