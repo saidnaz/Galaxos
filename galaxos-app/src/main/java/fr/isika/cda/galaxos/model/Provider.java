ac55f9022b368cbdd1817d688cacc894f4a6ef43
SAID <smatnaziha@yahoo.fr>
2021-12-31 14:22:12 +0100

    correction

--- galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Provider.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Provider.java
@@ -20,8 +20,8 @@ public class Provider {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long idProvider;
 	
-	@OneToOne(cascade = CascadeType.ALL)
-	private Adherent adherent;
+	//@OneToOne(cascade = CascadeType.ALL)
+	//private Adherent adherent;
 	
 	@OneToMany(mappedBy="provider")
 	List<Post> posts;
@@ -31,29 +31,21 @@ public class Provider {
 		
 	}
 	
-	public Provider(Long idProvider, Adherent adherent, List<Post> posts) {
+	public Provider(Long idProvider, List<Post> posts) {
 		super();
 		this.idProvider = idProvider;
-		this.adherent = adherent;
+		//this.adherent = adherent;
 		this.posts = posts;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(adherent, idProvider);
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Provider other = (Provider) obj;
-		return Objects.equals(adherent, other.adherent) && Objects.equals(idProvider, other.idProvider);
-	}
+
+	/*
+	 * @Override public boolean equals(Object obj) { if (this == obj) return true;
+	 * if (obj == null) return false; if (getClass() != obj.getClass()) return
+	 * false; Provider other = (Provider) obj; return Objects.equals(adherent,
+	 * other.adherent) && Objects.equals(idProvider, other.idProvider); }
+	 */
 
 	public Long getIdProvider() {
 		return idProvider;
@@ -63,13 +55,11 @@ public class Provider {
 		this.idProvider = idProvider;
 	}
 
-	public Adherent getAdherent() {
-		return adherent;
-	}
-
-	public void setAdherent(Adherent adherent) {
-		this.adherent = adherent;
-	}
+	/*
+	 * public Adherent getAdherent() { return adherent; }
+	 * 
+	 * public void setAdherent(Adherent adherent) { this.adherent = adherent; }
+	 */
 
 	public List<Post> getPosts() {
 		return posts;
