d0681ca0b43fb390f81ecb67ce9a051f989346b5
SAID <smatnaziha@yahoo.fr>
2021-12-31 14:01:15 +0100

    add folder

---
+++ galaxos-app/src/main/java/fr/isika/cda/viewModel/ConsumerForm.java
@@ -0,0 +1,60 @@
+package fr.isika.cda.viewModel;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Cette classe Consumer pour  tester le  Paiement est utilisée par la couche présentation
+ * pour mémoriser les informations saisies dans le formulaire de création de compte Consumer.
+ */
+public class ConsumerForm implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4575174343357389963L;
+
+	@NotEmpty(message = "Ne doit pas être vide")
+	@NotNull(message = "Ne doit pas être null")
+	@Email
+	private String email;
+	
+	@NotEmpty(message = "Ne doit pas être vide")
+	@NotNull(message = "Ne doit pas être null")
+	@Size(min = 1, max = 25, message = "Doit être entre 1 et 25 car.")
+	private String password;
+	
+
+
+	public ConsumerForm() {
+	}
+
+
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
