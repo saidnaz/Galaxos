1352f48f8dc6c02d6d3f09d764f1c33c94f1058d
tititanou <tania.rojas@edu.itescia.fr>
2022-01-02 20:52:31 +0100

    roles

---
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Consumer.java
@@ -0,0 +1,8 @@
+package fr.isika.cda.galaxos.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Consumer extends Role{
+
+}
--- galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Provider.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/model/Provider.java
@@ -4,21 +4,17 @@ package fr.isika.cda.galaxos.model;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="providers")
-public class Provider {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long idProvider;
+public class Provider extends Role{
+	
+	/*
+	 * @Id
+	 * 
+	 * @GeneratedValue(strategy = GenerationType.AUTO) private Long idProvider;
+	 */
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Adherent adherent;
@@ -33,7 +29,7 @@ public class Provider {
 	
 	public Provider(Long idProvider, Adherent adherent, List<Post> posts) {
 		super();
-		this.idProvider = idProvider;
+		//this.idProvider = idProvider;
 		this.adherent = adherent;
 		this.posts = posts;
 	}
@@ -42,6 +38,8 @@ public class Provider {
 	public int hashCode() {
 		return Objects.hash(adherent, idProvider);
 	}
+	
+	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -55,13 +53,11 @@ public class Provider {
 		return Objects.equals(adherent, other.adherent) && Objects.equals(idProvider, other.idProvider);
 	}
 
-	public Long getIdProvider() {
-		return idProvider;
-	}
-
-	public void setIdProvider(Long idProvider) {
-		this.idProvider = idProvider;
-	}
+	/*
+	 * public Long getIdProvider() { return idProvider; }
+	 * 
+	 * public void setIdProvider(Long idProvider) { this.idProvider = idProvider; }
+	 */
 
 	public Adherent getAdherent() {
 		return adherent;
