f95b11124ac12fdc0625e1d2b589987e9ae7e741
SAID <smatnaziha@yahoo.fr>
2022-01-08 00:47:46 +0100

    Exceptions PostRepository

---
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/exceptions/DAOException.java
@@ -0,0 +1,15 @@
+package fr.isika.cda.galaxos.exceptions;
+
+public class DAOException extends Exception {
+
+	public DAOException(Exception message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -70234932960567084L;
+
+}
