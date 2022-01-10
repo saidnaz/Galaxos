a45687e57eecc4a8c332c586eaf5df2b7ab739e9
SAID <smatnaziha@yahoo.fr>
2022-01-10 15:03:09 +0100

    no commit message

--- galaxos-app/src/main/java/fr/isika/cda/galaxos/service/PostService.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/service/PostService.java
@@ -1,149 +1,121 @@
 package fr.isika.cda.galaxos.service;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.Date;
-
 import java.util.Optional;
 
 import javax.ejb.Stateless;
-
 import javax.inject.Inject;
 import javax.persistence.EntityNotFoundException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.Part;
 
 import fr.isika.cda.galaxos.exceptions.DAOException;
 import fr.isika.cda.galaxos.model.Post;
-
 import fr.isika.cda.galaxos.repository.PostRepository;
 import fr.isika.cda.galaxos.viewmodel.PostForm;
 
 @Stateless
-public class PostService  implements  Serializable{
-	
-	
+public class PostService implements Serializable {
+
 	private static final long serialVersionUID = -8572276956761602694L;
-	private static final String PATH_DISK ="D:/Galaxos/src/main/webapp/";
+	private static final String PATH_DISK = "/src/main/webapp/uploads";
 	private static final int SIZE_BUFFER = 10240;
-	
+
 	@Inject
-	PostRepository PR = new PostRepository();
-	
+	PostRepository PR;
 
-
-	
-	
 	public Post create(PostForm Pform) {
-		
-		
-		Optional<Post> name= PR.findByNom(Pform.getNom());
-		
 
-		if( name.isPresent() ) {
-			        
+		Optional<Post> name = PR.findByNom(Pform.getNom());
+
+		if (name.isPresent()) {
+
 			throw new EntityNotFoundException("l'annonce existe déjà");
 		}
 
 		return PR.create(Pform);
-		}
-	
+	}
 
-	
-		public Optional<Post> findByNom(String n) {
-			return PR.findByNom(n);}
-		
-	
-	
-		
-		//upload File
-			public String saveFile(Part file, String folder) {
-				
-				String filename = file.getName().trim();
-				
-				String shortNameFile = filename.substring(0, filename.lastIndexOf("."));
-				String extensionFile = filename.substring(filename.lastIndexOf("."));
-				
-				filename = shortNameFile + new Date().getTime() + extensionFile;
-				
-				File newFile = Paths.get(PATH_DISK, folder, filename).toFile();
+	// upload File
+//	public String saveFile(Part file, String folder) {
+//
+//		String filename = file.getName().trim();
+//
+//		String shortNameFile = filename.substring(0, filename.lastIndexOf("."));
+//		String extensionFile = filename.substring(filename.lastIndexOf("."));
+//
+//		filename = shortNameFile + new Date().getTime() + extensionFile;
+//
+//		File newFile = Paths.get(PATH_DISK, folder, filename).toFile();
+//
+//		try (InputStream input = file.getInputStream();
+//				BufferedInputStream bufferIn = new BufferedInputStream(input);
+//				BufferedOutputStream bufferOut = new BufferedOutputStream(new FileOutputStream(newFile))) {
+//
+//			byte[] tampon = new byte[SIZE_BUFFER];
+//			int longueur = 0;
+//
+//			while ((longueur = bufferIn.read(tampon)) > 0) {
+//				bufferOut.write(tampon, 0, longueur);
+//			}
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		return folder + "/" + newFile.getName();
+//	}
 
-				try (InputStream input = file.getInputStream();
-					BufferedInputStream bufferIn = new BufferedInputStream(input);
-					BufferedOutputStream bufferOut = new BufferedOutputStream(new FileOutputStream(newFile))){
-					
-					byte[] tampon = new byte[SIZE_BUFFER];
-		            int longueur = 0;
-		            
-		            while ((longueur = bufferIn.read(tampon)) > 0) {
-		                bufferOut.write(tampon, 0, longueur);
-		            }
+	// Delete
+	public void delete(Post p) throws DAOException {
 
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-				return  folder + "/" + newFile.getName();}
-			
-				
-		
-	//Delete	
- 	public void delete(Post p) throws DAOException {
-	
- 		PR.deletePost(p);}
- 	
- 	//Update
- 	public Post update(Post p) {
-	return PR.update(p);
- 	}
-	
-	
+		PR.deletePost(p);
+	}
+
+	// Update
+	public Post update(Post p) {
+		return PR.update(p);
+	}
+
 	public Optional<Post> findById(Long id) {
-		return PR.findByIdPost(id);}
-	
-	
+		return PR.findByIdPost(id);
+	}
+
+	public Optional<Post> findByNom(String n) {
+		return PR.findByNom(n);
+	}
+
 }
-	
-		/*
-		 * public List<Post> postList() {
-		 * 
-		 * return ( PR.findAll(); }
-		 * 
-		 * 
-		 * public void updatePost(Post pt) { //PS.create(pt); }
-		 * 
-		 * 
-		 * public void deletePost(Long id) {
-		 * 
-		 * String post = PR.findById(id).get().getPoster(); String filename =
-		 * post.substring(30); File file = new
-		 * File("./src\\main\\webapp\\uploads\\"+filename); if (file.delete())
-		 * {System.out.println("file deleted");}else
-		 * {System.out.println("file not deleted");} PR.deleteById(id); }
-		 * 
-		 * 
-		 * public Post findbyId(Long id) { return ER.findById(id).get(); }
-		 * 
-		 * 
-		 * public Post findPostByName(String name) { return PR.findByName(name); }
-		 * 
-		 * 
-		 * public List<Post> filterPost(Domain category) { return
-		 * PR.filterByDomain(category); }
-		 * 
-		 * public List<Post> upcomingPosts() { List<Post> list= PR.upcomingPosts();
-		 * return list; }
-		 * 
-		 * 
-		 * public List<Post> passedPosts() { List<Post> list= ER.passedPosts(); return
-		 * list; }
-		 */
 
-
+/*
+ * public List<Post> postList() {
+ * 
+ * return ( PR.findAll(); }
+ * 
+ * 
+ * public void updatePost(Post pt) { //PS.create(pt); }
+ * 
+ * 
+ * public void deletePost(Long id) {
+ * 
+ * String post = PR.findById(id).get().getPoster(); String filename =
+ * post.substring(30); File file = new
+ * File("./src\\main\\webapp\\uploads\\"+filename); if (file.delete())
+ * {System.out.println("file deleted");}else
+ * {System.out.println("file not deleted");} PR.deleteById(id); }
+ * 
+ * 
+ * public Post findbyId(Long id) { return ER.findById(id).get(); }
+ * 
+ * 
+ * public Post findPostByName(String name) { return PR.findByName(name); }
+ * 
+ * 
+ * public List<Post> filterPost(Domain category) { return
+ * PR.filterByDomain(category); }
+ * 
+ * public List<Post> upcomingPosts() { List<Post> list= PR.upcomingPosts();
+ * return list; }
+ * 
+ * 
+ * public List<Post> passedPosts() { List<Post> list= ER.passedPosts(); return
+ * list; }
+ */
--- galaxos-app/src/main/java/fr/isika/cda/galaxos/viewmodel/PostForm.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/viewmodel/PostForm.java
@@ -1,104 +1,92 @@
 package fr.isika.cda.galaxos.viewmodel;
 
-
 import java.time.LocalDate;
-
-import javax.servlet.http.Part;
 
 import fr.isika.cda.galaxos.model.Domain;
 
-
 public class PostForm {
-	
-private String nom;
-	
 
-    private String Description;
-	
-	
-  //  private LocalDate DateStart;
-	
-   // private LocalDate DateEnd;
-    
-   
-    private Double price;
+	private String nom;
 
+	private Long idAdherent;
 	
-	private Part photo;
-	
-	
-	public PostForm(String nom, String description, LocalDate dateStart, LocalDate dateEnd, Double price,Part photo,
-			Domain domain) {
-		super();
-		this.nom = nom;
-		Description = description;
-		this.price = price;
-		this.photo = photo;
-		this.domain = domain;
+	private String Description;
+
+	private LocalDate startDate;
+
+	private LocalDate endDate;
+
+	private Double price;
+
+	private String photo;
+
+	public Long getIdAdherent() {
+		return idAdherent;
 	}
-
-
+	public void setIdAdherent(Long idAdherent) {
+		this.idAdherent = idAdherent;
+	}
+	
 	public String getNom() {
 		return nom;
 	}
-
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-
 	public String getDescription() {
 		return Description;
 	}
-
 
 	public void setDescription(String description) {
 		Description = description;
 	}
 
+	public LocalDate getStartDate() {
+		return startDate;
+	}
 
-//
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
 
+	public LocalDate getEndDate() {
+		return endDate;
+	}
 
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
 
 	public Double getPrice() {
 		return price;
 	}
 
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
 
-
-	public Part getPhoto() {
+	public String getPhoto() {
 		return photo;
 	}
 
-
-	public void setPhoto(Part photo) {
+	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
-
 
 	public Domain getDomain() {
 		return domain;
 	}
 
-
 	public void setDomain(Domain domain) {
 		this.domain = domain;
 	}
 
-
 	public PostForm() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-
 	private Domain domain;
-	
 
 }
