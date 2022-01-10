37df73416941cccb84d3dcb6234587ad0c2eb0b6
SAID <smatnaziha@yahoo.fr>
2022-01-10 15:00:20 +0100

    no commit message

--- galaxos-app/src/main/java/fr/isika/cda/galaxos/managedbeans/PostBean.java
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/managedbeans/PostBean.java
@@ -1,5 +1,8 @@
 package fr.isika.cda.galaxos.managedbeans;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Optional;
 
 import javax.faces.application.FacesMessage;
@@ -9,10 +12,12 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
+import fr.isika.cda.galaxos.helper.UploadHelper;
 import fr.isika.cda.galaxos.model.Adherent;
-import fr.isika.cda.galaxos.model.CompteUser;
-import fr.isika.cda.galaxos.model.Post;
+import fr.isika.cda.galaxos.model.Domain;
+import fr.isika.cda.galaxos.service.AdherentService;
 import fr.isika.cda.galaxos.service.PostService;
 import fr.isika.cda.galaxos.viewmodel.PostForm;
 
@@ -20,62 +25,124 @@ import fr.isika.cda.galaxos.viewmodel.PostForm;
 @ViewScoped
 public class PostBean {
 
-	
-	
-	@Inject
-	private PostService PS ;
-	
-	PostForm pform = new PostForm();
-	
-	
+	private String nom;
+
+	private String Description;
+
+	private Date startDate; // conversion a LocalDate
+
+	private Date endDate;
+
+	private Double price;
+
+	private Domain domain;
+	private Part photo;
+
+	private LocalDate convertTo(Date dateToConvert) {
+		return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Part getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Part photo) {
+		this.photo = photo;
+	}
+
+	public Domain getDomain() {
+		return domain;
+	}
+
+	public void setDomain(Domain domain) {
+		this.domain = domain;
+	}
+
 	public PostBean() {
 		super();
-		
-	
+
 	}
+
+	/////////////////////////////////////////////////////////
+
+	@Inject
+	private PostService PS;
+
+	PostForm pform = new PostForm();
+
+	@Inject
+	private AdherentService AS;
+
 	public String create() {
-		
-		
-		Adherent ad = new Adherent();
 		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-		Long id = (Long) session.getAttribute("Id");
+		Long id = (Long) session.getAttribute("connectedAdherentId");
+		Optional<Adherent> optional = AS.findById(id);
+
 		UIComponent formulaire = FacesContext.getCurrentInstance().getViewRoot().findComponent("createp");
-		
-		Optional <Post> optional = PS.findById(id);
-		
-			
-
-			if( optional.equals(null)) {
+		if (optional.isPresent()) {
+			Adherent adherent = optional.get();
+			try {
 				
-				FacesContext.getCurrentInstance().addMessage(formulaire.getClientId(), new FacesMessage());
+				System.out.println("adh : " + adherent);
 				
-				return "";
-			} else 			
-				try {
+				LocalDate startDateLocal = convertTo(startDate);
+				LocalDate endDateLocal = convertTo(endDate);
+				System.out.println("start : " + startDateLocal);
+				System.out.println("end : " + endDateLocal);
 
-			PS.create(pform);
-			return "listAnnonces?faces-redirect=true";
+				
+				
+				UploadHelper uploadHelper = new UploadHelper();
+				String photoFile = uploadHelper.processUpload(photo);
+				
+				pform = new PostForm();
+				pform.setNom(nom);
+				pform.setDescription(Description);
+				pform.setDomain(domain);
+				pform.setStartDate(startDateLocal);
+				pform.setEndDate(endDateLocal);
+				pform.setPhoto(photoFile);
+				pform.setPrice(price);
+				
+				pform.setIdAdherent(id);
+				PS.create(pform);
+				
+				return "listAnnonces?faces-redirect=true";
 
-		} catch (Exception ex) {
+			} catch (Exception ex) {
+				FacesContext.getCurrentInstance().addMessage("createp", new FacesMessage(ex.getMessage()));
 
+			}
 
-			FacesContext.getCurrentInstance().addMessage("forumaire", new FacesMessage(ex.getMessage()));
-			
-		} 
+		} else {
+			FacesContext.getCurrentInstance().addMessage(formulaire.getClientId(), new FacesMessage());
+			return "createAccount";
+		}
 		return "poster";
 	}
-	
-	}	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-
-
-
+}
