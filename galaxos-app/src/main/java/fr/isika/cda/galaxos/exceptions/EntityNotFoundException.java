9ac8a8768c0fd6d880066479c5b95fae451a4382
SAID <smatnaziha@yahoo.fr>
2022-01-10 10:23:32 +0100

    no commit message

---
+++ galaxos-app/src/main/java/fr/isika/cda/galaxos/exceptions/EntityNotFoundException.java
@@ -0,0 +1,17 @@
+package fr.isika.cda.galaxos.exceptions;
+
+public class EntityNotFoundException extends Exception{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 627564277451055184L;
+
+	public EntityNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
